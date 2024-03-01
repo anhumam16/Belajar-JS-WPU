7347eb7c12f70b6bf9bdcfbe21fe1267b1bd6f22
anhumam16 <anhumam16@gmail.com>
2024-03-01 20:45:52 +0700

    Belajar object literal dan function declaration

---
+++ object.js
@@ -0,0 +1,52 @@
+// Object Literal
+let santri1 = {
+    nama: "Herland",
+    power: 10,
+    ngaji: function(jam) {
+        this.power = this.power + jam;
+        console.log(`Halo ${this.nama}, powermu bertambah!`);
+    },
+    ngopi: function(lama) {
+        this.power = this.power - lama;
+        console.log(`Halo ${this.nama}, powermu berkurang!`);
+    }
+}
+
+let santri2 = {
+    nama: "Kazlan",
+    power: 10,
+    ngaji: function(jam) {
+        this.power = this.power + jam;
+        console.log(`Halo ${this.nama}, powermu bertambah!`);
+    },
+    ngopi: function(lama) {
+        this.power = this.power - lama;
+        console.log(`Halo ${this.nama}, powermu berkurang!`);
+    }
+}
+
+// function Declaration
+function Santri(nama, power) {
+    let santri = {};
+    santri.nama = nama;
+    santri.power = power;
+
+    santri.ngaji = function (jam) {
+        this.power += jam;
+        console.log(`Halo ${nama}, powermu bertambah`);
+    }
+
+    santri.ngopi = function (lama) {
+        this.ngopi -= lama;
+        console.log(`Halo ${nama}, powermu berkurang`);
+    }
+
+    return santri;
+}
+
+let Herland = Santri('Herland', 15);
+let Kazlan = Santri('Kazlan', 20);
+
+
+// Constructor Function 
+// Object.create
